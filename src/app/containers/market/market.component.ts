344274560a899c222ca17b9a316d4321cbc08ac8
RiggedToBlow <32483353+RiggedToBlow@users.noreply.github.com>
2020-05-06 22:21:50 +0300

    minor change

--- src/app/containers/market/market.component.ts
+++ src/app/containers/market/market.component.ts
@@ -7,13 +7,7 @@ import { Component, OnInit } from "@angular/core";
 })
 export class MarketComponent implements OnInit {
   products = [
-    {
-      title: "بسكليتة ظريفة كتير",
-      image:
-        "https://image.made-in-china.com/2f0j00kpIGTRefIHcB/Compertitive-Price-Bicycle-Kids-Bike-Popular-Style.jpg",
-      price: 500,
-      description: "بسكليتة يابانية اصلية بتمشي وبتركد واحيانا بتطلع عالسقف",
-    },
+
     {
       title: "بسكليتة ظريفة كتير",
       image:
