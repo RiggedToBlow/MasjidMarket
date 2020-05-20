38cde04ff896792c9d7f732e90283c4ce93a9a41
mouhand_kd <mouhand@protonmail.com>
2020-05-21 01:49:06 +0300

    added pagination to products view

--- back-end/masjidMarket/masjidMarket/market/views/products.py
+++ back-end/masjidMarket/masjidMarket/market/views/products.py
@@ -3,10 +3,11 @@ from django.views.generic import View
 from django.http import HttpResponse, JsonResponse
 from ..models import Product
 from ..authenticate import authenticateToken
+from django.core.paginator import Paginator
 
 
 class ProductsView(View):
-    def post(self, request):
+    def post(self, request, page_num):
         body = json.loads(request.body.decode("utf-8"))
         try:
             token = body.get('token')
@@ -15,6 +16,19 @@ class ProductsView(View):
         except:
             return HttpResponse(status=401)
 
-        products = list(Product.objects.all().values())
+
+        products_list = Product.objects.all()
+        paginator = Paginator(products_list, 2)
+        if page_num > paginator.num_pages:
+            return HttpResponse(status=400)
+
+        products = list()
+        for product in list(paginator.page(page_num)):
+            products.append({
+                "id": product.id,
+                "title": product.title,
+                "description": product.description,
+                "image": product.image,
+                "price": product.price })
 
         return JsonResponse(products, safe=False)
