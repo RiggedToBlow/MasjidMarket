d3ff93ae4512778c69c3b1fe1f0f05734596f275
Muhannad Abo Hajyah <groundgrind1@gmail.com>
2020-06-02 19:41:19 +0300

    made pagination work with categories

--- front-end/src/app/containers/market/market.component.ts
+++ front-end/src/app/containers/market/market.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { BehaviorSubject, combineLatest, Subscription } from "rxjs";
-import { map, take, pluck, tap } from "rxjs/operators";
+import { map, take, pluck, tap, switchMap, startWith } from "rxjs/operators";
 import { CartService } from "src/app/services/cart.service";
-import { FormControl } from '@angular/forms';
+import { FormControl } from "@angular/forms";
 
 @Component({
   selector: "app-market",
@@ -13,38 +13,33 @@ import { FormControl } from '@angular/forms';
 export class MarketComponent implements OnInit, OnDestroy {
   selectedProducts$ = this.cart.selectedProducts$;
   subscription$ = new Subscription();
-  products$ = this.cart.products$;
-  categorized$ = this.cart.categorizedProducts$
-  categories$ = this.categorized$.pipe(map(val=>Object.keys(val)))
-  selectedCategory = new FormControl("All")
+  categorized$ = this.cart.categorizedProducts$;
+  categories$ = this.categorized$.pipe(map((val) => Object.keys(val)));
+  selectedCategory = new FormControl("All");
+  products$ = this.selectedCategory.valueChanges.pipe(
+    startWith("All"),
+    switchMap((category) =>
+    category == "All"
+    ? this.cart.products$
+    : this.categorized$.pipe(pluck(category))
+    ),
+    tap(()=>this.currentPage$.next(1)),
+  );
   currentPage$ = new BehaviorSubject(1);
   PagesArray$ = this.products$.pipe(
-    map((products) => {
+    map((products:any) => {
       const pagesLength = Math.ceil(products.length / 9.0);
       return new Array(pagesLength).fill(0).map((val, index) => index + 1);
     })
   );
-  shownProducts$ = this.getShownProducts(this.products$)
+  shownProducts$ = combineLatest(this.products$, this.currentPage$).pipe(
+    map(([products, page]: any) => products.slice(9 * page - 9, 9 * page))
+  );
 
   constructor(private cart: CartService, private dialog: MatDialog) {}
 
-    getShownProducts(products){
-      return combineLatest(products, this.currentPage$).pipe(
-        map(([products, page]:any) => products.slice(9 * page - 9, 9 * page)),
-      );
-    }
-
   ngOnInit() {
     this.cart.getProducts();
-    this.subscription$.add(
-      this.selectedCategory.valueChanges
-      .subscribe(category=>{
-        if (category =="All")
-          this.shownProducts$ = this.getShownProducts(this.products$)
-        else
-          this.shownProducts$ = this.getShownProducts(this.categorized$.pipe(pluck(category)))
-      })
-    )
     this.subscription$.add(
       this.selectedProducts$.subscribe((ob: any) => {
         const arr = Object.values(ob);
@@ -66,11 +61,11 @@ export class MarketComponent implements OnInit, OnDestroy {
   onForward() {
     combineLatest(this.products$, this.currentPage$)
       .pipe(take(1))
-      .subscribe(([products, page]) => {
+      .subscribe(([products, page]:any) => {
         const length = products.length / 9.0;
         if (page <= length) {
           this.currentPage$.next(page + 1);
-          this.scrollToTop()
+          this.scrollToTop();
         }
       });
   }
@@ -78,29 +73,29 @@ export class MarketComponent implements OnInit, OnDestroy {
     this.currentPage$.pipe(take(1)).subscribe((page) => {
       if (page > 1) {
         this.currentPage$.next(page - 1);
-        this.scrollToTop()
+        this.scrollToTop();
       }
     });
   }
 
   onLastPage() {
-    this.products$.pipe(take(1)).subscribe((products) => {
+    this.products$.pipe(take(1)).subscribe((products:[]) => {
       const length = products.length / 9.0;
       this.currentPage$.next(Math.ceil(length));
-      this.scrollToTop()
+      this.scrollToTop();
     });
   }
   onFirstPage() {
     this.currentPage$.next(1);
-    this.scrollToTop()
+    this.scrollToTop();
   }
 
   changePage(page) {
     this.currentPage$.next(page);
-    this.scrollToTop()
+    this.scrollToTop();
   }
   // this is edit
-  scrollToTop(){
+  scrollToTop() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
